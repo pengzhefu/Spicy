894b232dc5653cf70121dddad9323f74144c16db
pengzhefu <32311379+pengzhefu@users.noreply.github.com>
2019-03-09 00:07:03 -0500

    Update WebCrawlerfrom0.py

--- WebCrawlerfrom0.py
+++ WebCrawlerfrom0.py
@@ -27,3 +27,15 @@ print(res)  ##如果成功返回200,不成功就是404，400之类的
 print(res.raise_for_status())
 print(res.text)
 
+##用try的方式
+url = 'http://bj.xiaozhu.com/'
+try:
+    #修改headers参数可以让爬虫模拟人操作浏览器的行为进行数据读取
+    r = requests.get(url, timeout = 60,headers = kv)#timeout时间是60s,如果是那种禁止爬虫的就可以这也模拟人用浏览器登录
+#    r = requests.get(url, timeout = 60)
+    r.raise_for_status()#看信息有没有获得，如果返回值是404就转到except
+    r.encoding = r.apparent_encoding#用内容的编码方式取代头部猜测的编码方式
+#        print(r.request.headers,'\n')#属性是request，库是requests
+    print(r.text)#返回内容 如果返回头部内容是r.headers
+except:
+     print("Something wrong")
